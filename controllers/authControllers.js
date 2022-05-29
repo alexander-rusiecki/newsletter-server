b7eacbcead0d806c0af8dd38327ba9c6910c36a4
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-05-29 08:35:08 +0200

    back to testing Heroku deploy

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -45,7 +45,7 @@ const signup = async (req, res) => {
 };
 
 const logout = (req, res) => {
-  res.clearCookie('jwt');
+  res.cookie('jwt', '', { maxAge: 1 });
   res.status(200).json({ msg: 'You are logged out, welcome back any time.' });
 };
 
